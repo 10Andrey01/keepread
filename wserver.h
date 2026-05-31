89d01f58dc8e107bac345f3c2204afc9a2cbdaa8
Andrey Kutin <adwet@yandex.ru>
2026-05-31 17:33:25 +0300

    add: added member QWebSocketServer in wserver

--- wserver.h
+++ wserver.h
@@ -1,14 +1,27 @@
 #ifndef WSERVER_H
 #define WSERVER_H
 
+#include <QByteArray>
+#include <QList>
 #include <QObject>
+#include <QWebSocketServer>
 
 class wserver : public QObject {
   Q_OBJECT
-public:
-  explicit wserver(QObject *parent = nullptr);
+  QWebSocketServer *m_pWebSocketServer = nullptr;
+  quint16 port = 3000;
+  QList<QWebSocket *> m_clients;
 
+public:
+  explicit wserver(quint16 port,
+                   QObject *parent = nullptr);
+public slots:
+  void onNewConnection();
+  void processTextMessage(QString message);
+  void processBinaryMessage(QByteArray message);
+  void socketDisconected();
 signals:
+  void closed();
 };
 
 #endif // WSERVER_H
